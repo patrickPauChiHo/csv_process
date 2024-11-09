356646fae506bdccae84bc8c17c49686b767483d
PatrickPau <patrickpauchiho@gmail.com>
2024-11-09 11:47:07 +1100

    build read_csv function

--- script.py
+++ script.py
@@ -1,2 +1,19 @@
 import csv
 import yaml
+
+#function to read csv file
+def read_csv(file_path):
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            print(row)
+            
+
+
+def main():
+    records = read_csv('input.csv')
+    
+    return records
+    
+if __name__ == '__main__':
+    main()
