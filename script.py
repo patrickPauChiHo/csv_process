b12b169e7f95c8f2d7f37a0fc3cc3d362dd19f6f
PatrickPau <patrickpauchiho@gmail.com>
2024-11-09 11:58:56 +1100

    add the sorted_records function

--- script.py
+++ script.py
@@ -4,16 +4,19 @@ import yaml
 #function to read csv file
 def read_csv(file_path):
     with open(file_path, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
-            print(row)
+        #use the csv.DictReader to read the csv file to a dictionary
+        csv_reader = csv.DictReader(csv_file)
+        records=[row for row in csv_reader]
+    return records
             
 
 
 def main():
     records = read_csv('input.csv')
+    sorted_records = sorted(records, key=lambda x: (int(x['division']), -int(x['points'])))
+    print(sorted_records)
     
-    return records
+    
     
 if __name__ == '__main__':
     main()
